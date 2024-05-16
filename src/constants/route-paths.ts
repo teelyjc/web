8862b3b3cb08e288e5f61bcf9639c3d2b5d6e7d5
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-05-16 20:44:52 +0700

    feat: add routes constant

---
+++ src/constants/route-paths.ts
@@ -0,0 +1,11 @@
+export const RoutePath = {
+  HOME: '/',
+  SIGNIN: '/signin',
+  SIGNUP: '/signup',
+
+  FORGOT_PASSWORD: '/forgot-password',
+  CHANGE_PASSWORD: '/change-password',
+
+  DASHBOARD: '/dashboard',
+  SETTINGS: '/settings',
+};
