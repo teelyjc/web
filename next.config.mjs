22330dc26cb3de8e0ff084ea6a6b5da743cf225c
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-05-16 20:37:05 +0700

    feat: add runtime config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  publicRuntimeConfig: {
+    BACKEND_URL: "http://localhost:4000",
+    WS_URL: "ws://localhost:4000/ws"
+  }
 };
 
 export default nextConfig;
