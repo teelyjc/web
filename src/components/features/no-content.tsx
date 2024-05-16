19461f0600be078c2afe643f20a2d64849207d17
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-05-16 20:44:35 +0700

    feat: add blank components

---
+++ src/components/features/no-content.tsx
@@ -0,0 +1,6 @@
+/**
+ * @description Next.js required a component. to make a page visible and usecase of no content.
+ */
+export function NoContent() {
+  return 'No Content, Just a redirect or something. (╯°□°)╯︵ ┻━┻';
+}
